27c0bbd8533ec5d3c3e846076dfc191bf018c3a5
yuyasat <takiyour@gmail.com>
2023-05-20 22:32:43 +0900

    vue3 test until Provide/Inject

---
+++ section10/vue3-test/src/components/GrandChildren.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>GrandChildren（孫）</div>
+  <div>{{ userName }}</div>
+</template>
+
+<script>
+export default {
+  data() {},
+  inject: ["userName"],
+};
+</script>
+
+<style></style>
---
+++ section10/vue3-test/src/views/ChildrenView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Children（子）</div>
+  <Grand-Children />
+</template>
+
+<script>
+import GrandChildren from "@/components/GrandChildren";
+export default {
+  components: {
+    GrandChildren,
+  },
+};
+</script>
+
+<style></style>
