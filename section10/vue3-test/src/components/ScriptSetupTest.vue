a2235807031635952172f3ebb87d3aeaaaba1b91
yuyasat <takiyour@gmail.com>
2023-05-22 21:31:51 +0900

    vue3 props, emmit

---
+++ section10/vue3-test/src/components/ScriptSetupTest.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, defineProps, defineEmits } from "vue";
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+const emitTest = defineEmits(["custom-event"]);
+const count = ref(0);
+const increment = () => {
+  count.value++;
+};
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <div>ScriptSetup</div>
+  <div>{{ count }}</div>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+  <p>{{ props.title }}</p>
+  <button @click="emitTest('custom-event', '子からの値')">Emitテスト</button>
+</template>
+
+<style></style>
--- section10/vue3-test/src/views/HomeView.vue
+++ section10/vue3-test/src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <ScriptSetupTest
+      title="ここにタイトルが入ります"
+      @custom-event="parentMethod"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import ScriptSetupTest from "@/components/ScriptSetupTest.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    ScriptSetupTest,
+  },
+  methods: {
+    parentMethod(e) {
+      console.log("Emit実行", e);
+    },
+  },
+};
 </script>
